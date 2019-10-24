62d692c117cf10b8a6c3ae7f691b30977a6caf94
ferryrules <ferris@ferryrules.com>
2019-10-24 10:25:33 -0400

    api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Dropdown } from 'semantic-ui-react'
 import './App.css';
 
+const API = 'https://content-staging.prompt.com/api/data/university/'
+const API_KEY = process.env.REACT_APP_API_KEY
+const options = [
+  { key: 1, text: 'Michigan State University', value: 171100 },
+  { key: 2, text: 'Georgetown', value: 131496 },
+  { key: 3, text: 'Prompt University', value: 98765 },
+]
+
+const selectSchool = async(e) => {
+  const school = await fetch(`${API}${options[0]['value']}/?identifierType=iped`, {
+    headers: {
+      'Authorization': `Token ${API_KEY}`
+    }
+  })
+  .then(r=>r.json())
+  console.log(school)
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Dropdown clearable options={options} selection onChange={(e)=>selectSchool(e)}/>
       </header>
     </div>
   );
